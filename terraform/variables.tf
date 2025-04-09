c1192d83e758cdf21b9726b4d954b98eb42c8835
Avashneupane9857 <juisch20220011252@gmail.com>
2025-04-09 16:58:44 +0530

    Update tf files and notes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,13 @@
 variable "aws_region" {
   description = "AWS region where resources will be provisioned"
-  default     = "us-east-2"
+  default     = "ap-southeast-2"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-085f9c64a9b75eed5"
+  default     = "ami-0f5d1713c9af4fe30"
 }
+
 
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
