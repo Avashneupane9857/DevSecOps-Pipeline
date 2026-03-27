1599d94ed901c2f80c76e3fe6c19524350042304
Avashneupane9857 <juisch20220011252@gmail.com>
2026-03-27 18:35:13 +0000

    Update tf files and notes

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "deployer" {
-  key_name   = "terra-automate-key"
-  public_key = file("/Users/shubham/Documents/work/TrainWithShubham/terra-practice/terra-key.pub")
+  key_name   = "terra-automate-key" 
+  public_key = file("/Users/avashneupane/DevSecOps-Pipeline/terraform/terraform-key.pub")
 }
 
 resource "aws_default_vpc" "default" {
